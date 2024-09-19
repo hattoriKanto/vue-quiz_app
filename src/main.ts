68aa2d67ca70a2d84b692958b339f41cd43caa5b
Ihor Bahniuk <hattorikanto@gmail.com>
2024-09-20 01:01:08 +0300

    added normalize.css & created reset.css

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import 'normalize.css/normalize.css'
+import './styles/reset.css'
+
 import App from './App.vue'
 
 const app = createApp(App)
