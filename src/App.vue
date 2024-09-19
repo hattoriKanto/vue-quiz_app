c835df18252ff4b015fca353fdb544ec6e8a2208
Ihor Bahniuk <hattorikanto@gmail.com>
2024-09-20 00:57:00 +0300

    added category section

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,47 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CategoryComponent from './components/CategoryComponent/CategoryComponent.vue'
+</script>
 
-<template></template>
+<template>
+  <main class="main">
+    <div class="container">
+      <section class="categories">
+        <div class="categories__wrapper">
+          <h1 class="categories__title">Choose category to start:</h1>
+          <div class="categories__cards">
+            <CategoryComponent />
+            <CategoryComponent />
+            <CategoryComponent />
+            <CategoryComponent />
+            <CategoryComponent />
+            <CategoryComponent />
+            <CategoryComponent />
+            <CategoryComponent />
+            <CategoryComponent />
+          </div>
+        </div>
+      </section>
+    </div>
+  </main>
+</template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  margin: 0 auto;
+  padding-inline: 12px;
+  max-width: 1200px;
+}
+
+.categories__wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.categories__cards {
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 12px;
+}
+</style>
